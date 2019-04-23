c2c88e7ed2c10ff1b14b54260f2b20e70e19911a
Matthias Bussonnier <mbussonnier@ucmerced.edu>
2019-04-23 12:01:16 -0700

    more error handling and logging

--- sbatch_wrapper/__init__.py
+++ sbatch_wrapper/__init__.py
@@ -21,39 +21,45 @@ def helper(argv, *, call_sbatch=call_sbatch):
         sub_script = argv[1:]
 
     result = call_sbatch(sub_script)
-    used_wallclock = False
-    used_exclusive = False
-    if not sub_script:
-        sys.exit(sub_script)
-    with open(sub_script[0]) as f:
-        for l in f.readlines():
-            if not l.startswith("#SBATCH"):
-                continue
-            if "--exclusive" in l:
-                used_exclusive = True
-            if "-t" in l:
-                used_wallclock = True
-                # To-do: If user doesn't request for all the cores on the node on full.q, do not let them submit the job
-            if "full.q" in l:
-                print(
-                    "You are submitting your job to full.q without requesting for full node. Please use this flag IF and ONLY IF you are using all the cores on the node"
-                )
-
+    try:
+        used_wallclock = False
+        used_exclusive = False
+        if not sub_script:
+            sys.exit(sub_script)
+        with open(sub_script[0]) as f:
+            for l in f.readlines():
+                if not l.startswith("#SBATCH"):
+                    continue
+                if "--exclusive" in l:
+                    used_exclusive = True
+                if "-t" in l:
+                    used_wallclock = True
+                    # To-do: If user doesn't request for all the cores on the node on full.q, do not let them submit the job
+                # if "full.q" in l:
+                #     print(
+                #         "You are submitting your job to full.q without requesting for full node. Please use this flag IF and ONLY IF you are using all the cores on the node"
+                #     )
+    except:
+        pass
     stdout = result.stdout.decode()
     print(stdout, end="")
-    jid = stdout.split(" ")[-1].strip()
-    print(result.stderr.decode(), end="")
+    print(result.stderr.decode(), end="", file=sys.stderr)
 
+    jid = stdout.split(" ")[-1].strip()
     return jid, used_wallclock, used_exclusive
 
 
 def main(argv, *, call_sbatch=call_sbatch):
 
     jid, used_wallclock, used_exclusive = helper(argv, call_sbatch=call_sbatch)
-
     if used_exclusive:
-        with open("exclusive_jobs", "a") as f:
-            f.write("Job" + str(jid) + "was submitted with exclusive")
+        try:
+            with open("/var/log/sbatch.log", "a") as f:
+                import json
+                f.write(json.dumps({"job_id": jid, 
+                                    "exclusive" : true}))
+        except:
+            pass
         print(
             "WARNING: You are using --exclusive flag in your submission file. This blocks other users from running jobs on the same node as your job. Please use this flag IF and ONLY IF you are absolutely sure you need an entire node"
         )
